deed93251daf7898db5abef40d97ca7be33cd44a
Maria Ramirez <nanyramirez0@gmail.com>
2016-12-24 19:08:59 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,29 @@
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pigeon_list={}
+ data.each do |prop, attribute|
+    attribute.each do |attribute, pigeons|
+      pigeons.each do |pigeon|
+        pigeon_list[pigeon]={}
+        end
+      end
+     end
+
+  pigeon_list.each do |pigeon, properties|
+    data.each do |prop, attribute|
+      pigeon_list[pigeon][prop]=[]
+    end
+  end
+
+  data.each do |key, first_tier_value|
+    first_tier_value.each do |second_tier_key, pigeon_array|
+      pigeon_list.each_key do |pigeon|
+        if pigeon_array.include?(pigeon)
+          pigeon_list[pigeon][key].push(second_tier_key.to_s)
+        end
+      end
+    end
+  end
+
+  pigeon_list
 end
